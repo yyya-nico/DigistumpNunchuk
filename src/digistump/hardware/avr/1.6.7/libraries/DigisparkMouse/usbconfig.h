9667bd60e771163b704c0c28d29176cbd46743a4
yyya_nico <hirano4650026@gmail.com>
2023-11-13 03:22:47 +0900

    デバイス名など変更

--- src/digistump/hardware/avr/1.6.7/libraries/DigisparkMouse/usbconfig.h
+++ src/digistump/hardware/avr/1.6.7/libraries/DigisparkMouse/usbconfig.h
@@ -244,8 +244,8 @@ section at the end of this file).
 #define USB_CFG_DEVICE_VERSION  0x00, 0x01
 /* Version number of the device: Minor number first, then major number.
  */
-#define USB_CFG_VENDOR_NAME     'd','i','g','i','s','t','u','m','p','.','c','o','m'
-#define USB_CFG_VENDOR_NAME_LEN 13
+#define USB_CFG_VENDOR_NAME     'y','y','y','a','-','n','i','c','o','.','c','o'
+#define USB_CFG_VENDOR_NAME_LEN 12
 /* These two values define the vendor name returned by the USB device. The name
  * must be given as a list of characters under single quotes. The characters
  * are interpreted as Unicode (UTF-16) entities.
@@ -254,14 +254,14 @@ section at the end of this file).
  * obdev's free shared VID/PID pair. See the file USB-IDs-for-free.txt for
  * details.
  */
-#define USB_CFG_DEVICE_NAME     'D','i','g','i','K','e','y'
-#define USB_CFG_DEVICE_NAME_LEN 7
+#define USB_CFG_DEVICE_NAME     'N','u','n','c','h','u','k',' ','M','o','u','s','e'
+#define USB_CFG_DEVICE_NAME_LEN 13
 /* Same as above for the device name. If you don't want a device name, undefine
  * the macros. See the file USB-IDs-for-free.txt before you assign a name if
  * you use a shared VID/PID.
  */
-#define USB_CFG_SERIAL_NUMBER   'd','i','g','i','s','t','u','m','p','.','c','o','m',':','M','o','u','s','e'
-#define USB_CFG_SERIAL_NUMBER_LEN   19
+#define USB_CFG_SERIAL_NUMBER   'y','y','y','a','-','n','i','c','o','.','c','o',':','M','o','u','s','e'
+#define USB_CFG_SERIAL_NUMBER_LEN   18
 /* Same as above for the serial number. If you don't want a serial number,
  * undefine the macros.
  * It may be useful to provide the serial number through other means than at
